9b0f76a60d7b0d0665fd8384ce5528c1fc371120
Rooman <Rooman@Rooman-PC>
2018-03-01 15:06:24 +0530

    first change done and given commit

--- HelloWeb3/src/com/tutorialspoint/HelloController.java
+++ HelloWeb3/src/com/tutorialspoint/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.ModelMap;
 @RequestMapping("/hello")
 public class HelloController {
    @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
-      model.addAttribute("message", "Hello Spring MVC Framework!");
+      model.addAttribute("message", "Hello Spring MVC Framework! by myself");
       return "hello";
    }
 }
